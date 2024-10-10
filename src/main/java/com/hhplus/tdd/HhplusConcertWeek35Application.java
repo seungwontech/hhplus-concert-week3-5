0f459b4eaad506f5924761be103ff67051ee5085
seungwon <swlee173@gmail.com>
2024-10-11 00:56:06 +0900

    init

---
+++ src/main/java/com/hhplus/tdd/HhplusConcertWeek35Application.java
@@ -0,0 +1,13 @@
+package com.hhplus.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusConcertWeek35Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusConcertWeek35Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/tdd/HhplusConcertWeek35ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConcertWeek35ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
