44ca09fdb2e098907b30dbe42cc38e6ef6d80e3f
seungwon <swlee173@gmail.com>
2024-10-21 20:50:39 +0900

    refactor: 예약 가능한 날짜 API 로직 및 단위 테스트 수정

--- src/main/java/com/hhplus/tdd/concert/application/usecase/GetAvailableConcertDatesUseCase.java
+++ src/main/java/com/hhplus/tdd/concert/application/usecase/GetAvailableConcertDatesUseCase.java
@@ -3,9 +3,9 @@ package com.hhplus.tdd.concert.application.usecase;
 import com.hhplus.tdd.concert.domain.model.Concert;
 import com.hhplus.tdd.concert.domain.model.ConcertSchedule;
 import com.hhplus.tdd.concert.domain.model.ConcertSeat;
-import com.hhplus.tdd.concert.domain.service.ConcertService;
-import com.hhplus.tdd.concert.exception.ConcertErrorResult;
-import com.hhplus.tdd.concert.exception.ConcertException;
+import com.hhplus.tdd.concert.domain.repository.ConcertRepository;
+import com.hhplus.tdd.concert.domain.repository.ConcertScheduleRepository;
+import com.hhplus.tdd.concert.domain.repository.ConcertSeatRepository;
 import com.hhplus.tdd.concert.presentation.response.ScheduleRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,15 +19,25 @@ import java.util.Map;
 @Service
 public class GetAvailableConcertDatesUseCase {
 
-    private final ConcertService concertService;
+    private final ConcertRepository concertRepository;
+    private final ConcertScheduleRepository concertScheduleRepository;
+    private final ConcertSeatRepository concertSeatRepository;
 
     public ScheduleRes execute(Long concertId) {
 
-        Concert concert = concertService.getConcert(concertId);
+        Concert concert = concertRepository.getConcert(concertId);
+        List<ConcertSchedule> schedules = concertScheduleRepository.getConcertSchedules(concertId);
+        List<ConcertSeat> seats = concertSeatRepository.getConcertSeats(concertId);
 
-        List<ConcertSchedule> schedules = concertService.getConcertSchedules(concertId);
-
-        List<ConcertSeat> seats = concertService.getConcertSeats(concertId);
+        if (concert == null) {
+            throw new IllegalArgumentException("콘서트가 존재하지 않습니다.");
+        }
+        if (schedules.isEmpty()) {
+            throw new IllegalArgumentException("날짜가 존재하지 않습니다.");
+        }
+        if (seats.isEmpty()) {
+            throw new IllegalArgumentException("좌석이 존재하지 않습니다.");
+        }
 
         Map<Long, Integer> reservedSeatsMap = countReservedSeatsByScheduleId(seats);
 
@@ -36,7 +46,8 @@ public class GetAvailableConcertDatesUseCase {
         return buildScheduleResponse(concert, availableSchedules);
     }
 
-    private Map<Long, Integer> countReservedSeatsByScheduleId(List<ConcertSeat> seats) {
+    // 콘서트 일정별로 예약된 좌석 수를 계산하는 메서드.
+    public Map<Long, Integer> countReservedSeatsByScheduleId(List<ConcertSeat> seats) {
         Map<Long, Integer> reservedSeatsMap = new HashMap<>();
         for (ConcertSeat seat : seats) {
             if ("Y".equals(seat.getReserveYn())) {
@@ -47,7 +58,8 @@ public class GetAvailableConcertDatesUseCase {
         return reservedSeatsMap;
     }
 
-    private List<ScheduleRes.Schedule> filterAvailableSchedules(List<ConcertSchedule> schedules, Map<Long, Integer> reservedSeatsMap) {
+    // 사용 가능한 콘서트 일정을 필터링하는 메서드.
+    public List<ScheduleRes.Schedule> filterAvailableSchedules(List<ConcertSchedule> schedules, Map<Long, Integer> reservedSeatsMap) {
         List<ScheduleRes.Schedule> scheduleList = new ArrayList<>();
         for (ConcertSchedule schedule : schedules) {
             int reservedSeats = reservedSeatsMap.getOrDefault(schedule.getConcertScheduleId(), 0);
@@ -63,9 +75,9 @@ public class GetAvailableConcertDatesUseCase {
         return scheduleList;
     }
 
-    private ScheduleRes buildScheduleResponse(Concert concert, List<ScheduleRes.Schedule> scheduleList) {
+    public ScheduleRes buildScheduleResponse(Concert concert, List<ScheduleRes.Schedule> scheduleList) {
         if (scheduleList.isEmpty()) {
-            throw new ConcertException(ConcertErrorResult.CONCERT_SCHEDULE_AVAILABLE_NOT_FOUND);
+            throw new IllegalArgumentException("콘서트의 사용 가능한 일정 없습니다.");
         } else {
             return ScheduleRes.of(concert.getConcertId(), scheduleList);
         }
