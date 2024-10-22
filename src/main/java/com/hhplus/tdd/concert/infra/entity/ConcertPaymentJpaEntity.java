d3f3a91f551008e0eda4fd79476314702a135849
seungwon <swlee173@gmail.com>
2024-10-22 12:25:01 +0900

    refactor: JpaEntity 컬럼에 name 속성 추가

--- src/main/java/com/hhplus/tdd/concert/infra/entity/ConcertPaymentJpaEntity.java
+++ src/main/java/com/hhplus/tdd/concert/infra/entity/ConcertPaymentJpaEntity.java
@@ -12,21 +12,22 @@ import java.time.LocalDateTime;
 public class ConcertPaymentJpaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
     private Long paymentId; // 결제 ID
 
-    @Column(nullable = false)
+    @Column(name = "user_id", nullable = false)
     private Long userId; // 사용자 ID
 
-    @Column(nullable = false)
+    @Column(name = "concert_reservation_id", nullable = false)
     private Long concertReservationId; // 콘서트 예약 ID
 
-    @Column(nullable = false)
+    @Column(name = "payment_amount", nullable = false)
     private int paymentAmount; // 결제 금액
 
-    @Column(nullable = false)
+    @Column(name = "payment_status", nullable = false)
     private String paymentStatus; // 결제 상태
 
-    @Column(nullable = false)
+    @Column(name = "payment_date", nullable = false)
     private LocalDateTime paymentDate; // 결제 날짜
 
     public static ConcertPaymentJpaEntity of(Long userId, Long concertReservationId, int paymentAmount, String paymentStatus, LocalDateTime paymentDate) {
