3eb8b8d7e2e71d6ea83bb0a5d7bd94008e51777e
seungwon <swlee173@gmail.com>
2024-10-22 18:06:02 +0900

    feat: 잔액 충전 및 잔액 조회 API 통합테스트 작성

---
+++ src/test/java/com/hhplus/tdd/balance/BalanceServiceIntegrationTest.java
@@ -0,0 +1,66 @@
+package com.hhplus.tdd.balance;
+
+import com.hhplus.tdd.balance.domain.model.Balance;
+import com.hhplus.tdd.balance.domain.repository.BalanceRepository;
+import com.hhplus.tdd.balance.domain.service.BalanceService;
+import com.hhplus.tdd.balance.presentation.response.BalanceRes;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class BalanceServiceIntegrationTest {
+
+    @Autowired
+    private BalanceRepository balanceRepository;
+
+    @Autowired
+    private BalanceService balanceService;
+
+    @Test
+    public void 포인트조회() {
+        // given
+        Long userId = 1L;
+        // when
+        BalanceRes res = balanceService.getBalance(userId);
+
+        // then
+        assertThat(res).isNotNull();
+        assertThat(res.getUserId()).isEqualTo(userId);
+    }
+
+    @Test
+    public void 포인트사용성공() {
+        // given
+        Long userId = 1L;
+        int amount = 500;
+        // when
+        BalanceRes res = balanceService.use(userId, amount);
+
+        Balance balance = balanceRepository.getBalance(userId);
+
+        // then
+        assertThat(res).isNotNull();
+        assertThat(res.getUserId()).isEqualTo(userId);
+        assertThat(balance.getBalanceAmount()).isEqualTo(res.getBalanceAmount());
+    }
+
+
+    @Test
+    public void 포인트충전성공() {
+        // given
+        Long userId = 1L;
+        int amount = 500;
+        // when
+        BalanceRes res = balanceService.charge(userId, amount);
+
+        Balance balance = balanceRepository.getBalance(userId);
+
+        // then
+        assertThat(res).isNotNull();
+        assertThat(res.getUserId()).isEqualTo(userId);
+        assertThat(balance.getBalanceAmount()).isEqualTo(res.getBalanceAmount());
+    }
+}
